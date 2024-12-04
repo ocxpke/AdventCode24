d4635286a7d9f97d960447191395e43e47b73b62
ocxpke <joseramirez0707@gmail.com>
2024-12-04 01:53:35 +0100

    Dia 3 todo

--- Day3/Day3.c
+++ Day3/Day3.c
@@ -3,51 +3,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *ft_substr(const char *s, unsigned int start, size_t len) {
-  size_t cont;
-  size_t len_s;
-  char *substr;
-
-  cont = 0;
-  len_s = strlen(s);
-  if (len > len_s)
-    len = len_s;
-  if (start > len_s)
-    len = 0;
-  if (len > (len_s - start))
-    len = len_s - start;
-  substr = (char *)calloc(1, len + 1);
-  if (!substr)
-    return (NULL);
-  if (!(*s) || start > len_s)
-    return (substr);
-  while (cont < len && s[start + cont]) {
-    substr[cont] = s[start + cont];
-    cont++;
-  }
-  substr[cont] = 0;
-  return (substr);
-}
-
 int isMul(char *str) {
   int ret = 0, x, y;
   int cont = 0, sum;
+  int multi = 1;
   while (str[cont]) {
     sum = 0;
+    if (!strncmp(&str[cont], "don't()", 7)) {
+      multi = 0;
+      cont += 7;
+    } else if (!strncmp(&str[cont], "do()", 4)) {
+      multi = 1;
+      cont += 4;
+    }
     if (!strncmp(&str[cont + sum], "mul(", 4)) {
+      /**
+       * All the content of this if could be replaced with the use of
+       * the function sscanf.
+       */
       sum += 4;
       x = atoi(&str[cont + sum]);
-      printf("hola %d\n", x);
       while (isdigit(str[cont + sum]) && str[cont + sum])
         sum++;
       if (str[cont + sum] == ',') {
         sum++;
         y = atoi(&str[cont + sum]);
-        printf("hola %d\n", y);
         while (isdigit(str[cont + sum]) && str[cont + sum])
           sum++;
-        if (str[cont + sum] == ')') {
+        if ((str[cont + sum] == ')') && multi) {
           ret += (x * y);
+          cont += sum;
         }
       }
     }
@@ -57,6 +42,8 @@ int isMul(char *str) {
 }
 
 int main(int argc, char **argv) {
+  if (argc < 2)
+    return (1);
   printf("El resultado de la multiplicacion es: %d\n", isMul(argv[1]));
   return (0);
 }
